3456800fb1fec744f5ed0f1be58a7294cafa535c
jiamuyu-server <Yjiamuyu@outlook.com>
2024-10-31 23:06:48 +0800

    二进制加法

--- lcof2/剑指 Offer II 002. 二进制加法/Solution.cpp
+++ lcof2/剑指 Offer II 002. 二进制加法/Solution.cpp
@@ -12,3 +12,52 @@ public:
         return ans;
     }
 };
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string ans;
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        for(int carry = 0; i >= 0 || j >= 0 || carry; i--, j--)
+        {
+            carry += (i > -1 ? (a[i] - '0') : 0) + (j > -1 ? ( b[j] - '0') : 0);
+            switch(carry)
+            {
+                case 0:
+                    ans.push_back('0');
+                    break;
+                case 1:
+                    ans.push_back('1');
+                    carry = 0;
+                    break;
+                case 2:
+                    ans.push_back('0');
+                    carry = 1;
+                    break;
+                case 3:
+                    ans.push_back('1');
+                    carry = 1;
+                    break;
+            }
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution solution;
+
+    string a = "11";
+    string b = "10";
+    string result = solution.addBinary(a, b);
+    cout << "The sum of " << a << " and " << b << " is: " << result << endl;
+    return 0;
+}
